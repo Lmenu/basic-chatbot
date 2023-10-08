3840fdb5878f9b36faf2952f0f4fdc11e725e3a0
Lmenu <108467489+Lmenu@users.noreply.github.com>
2023-10-08 18:09:41 +0530

    chatbot
    vvvvvv

---
+++ main.py
@@ -0,0 +1,30 @@
+import nltk
+from nltk.chat.util import Chat, reflections
+
+pairs = [
+    ["hello|hi|hey", ["Hello!", "Hi there!"]],
+    ["how are you?", ["I'm just a computer program, but I'm functioning well. How can I assist you today?"]],
+    ["what's your name?", ["I'm just a chatbot, so I don't have a name. How can I help you?"]],
+    ["bye|goodbye", ["Goodbye!", "Have a great day!"]],
+    ["Are you AI?", ["No, I am a chatbot!", "No!"]],
+    
+    
+]
+
+chatbot = Chat(pairs, reflections)
+
+def main():
+    print("Hello! How can I assist you today?")
+    while True:
+        user_input = input(">>> ")
+        if user_input.lower() == 'quit':
+            break
+        response = chatbot.respond(user_input)
+        print(response)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
